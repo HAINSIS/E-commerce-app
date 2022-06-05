51bdfecd6a39c9de7b0a3b9dbe7a61fff9ebc75d
HAINSIS <abdelhamid.hadili@gmail.com>
2022-06-05 19:31:34 +0100

    style the layout and enable it to render on the server side

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,24 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
+import useStyles from '../utils/style';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
-        <h1>Bleu Horizon</h1>
+        <title>Bleu Horizon</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navebar}>
         <Toolbar>
           <Typography>Bleu Horizon</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>all right reserved. Blu Horizon</footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        all right reserved. Blue Horizon
+      </footer>
     </div>
   );
 }
